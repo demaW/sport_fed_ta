1f0c186ab36eda1408024d12dc55d1dced81e1b8
Yaroslav <slav4yk@gmail.com>
2014-10-08 14:06:47 +0300

    test commit

--- lib/elements/abstract_element.rb
+++ lib/elements/abstract_element.rb
@@ -1,12 +1,35 @@
 class AbstractElement
-  def initialize(wrappedElement, name, page)
-    @wrappedElement= wrappedElement
+
+  def initialize(wrapped_element, name, page)
+    @wrapped_element = wrapped_element
     @name = name
     @page = page
   end
 
 
 
+  def is_displayed
+    if @wrapped_element.nil?
+      @res = @wrapped_element.displayed?
+      print "element #{@name}: "
+      puts(status  ? 'DISPLAYED' : 'NOT DISPLAYED ')
+      return @res
+    else
+      return false
+    end
+  end
 
+
+
+
+
+
+  def get_name
+    @name
+  end
+
+  def get_page
+    @page
+  end
 
 end

112ae1382820b1f457b26dd3bfe4867e57775905
demaW <demavov@gmail.com>
2014-10-10 18:05:05 +0300

    logger fix

--- lib/utils/log.rb
+++ lib/utils/log.rb
@@ -1,11 +1,9 @@
 require 'logging'
 class LoggerSF
-  $logger = Logging.logger
+  $logger = Logging.logger[STDOUT]
   $logger.add_appenders(
-      Logging.appenders.stdout,
+      Logging.appenders.stdout(),
       Logging.appenders.file('../log.log')
   )
-  Logging::Config::YamlConfigurator.load('sf_logging.yaml')
-
   $logger.level = :info
 end

d8854c96eb6740f341f170cece178083158e20ae
Yaroslav <slav4yk@gmail.com>
2014-10-13 13:12:58 +0300

    edit log path in require_relative

--- lib/business/home_page_bo.rb
+++ lib/business/home_page_bo.rb
@@ -1,13 +1,16 @@
 require_relative '../uimap/home_page_ui'
 require 'logger'
-#require_relative '../utils/logger'
+require_relative '../utils/log'
 class HomePageBO
   def initialize()
     @home_page = HomePageUI.new
-#    $logger.info "#{self.inspect}"
+    $logger.info "#{self.inspect}"
   end
 
   def exist
     @home_page.exist
   end
 end
+
+home = HomePageBO.new
+puts home.exist

6783426fa2e3c846dacb1afbeaf4afebdded7de3
Yaroslav <slav4yk@gmail.com>
2014-10-13 15:48:36 +0300

    add init methods

--- lib/uimap/home_page_ui.rb
+++ lib/uimap/home_page_ui.rb
@@ -1,9 +1,12 @@
 require_relative('base_page')
+require_relative('../../lib/elements/abstract_element')
 
 class HomePageUI < BasePage
   attr_accessor :hash_elements
+
   def initialize
-    @driver = super.driver
+    @driver = Selenium::WebDriver.for :firefox
+#    @driver = super.driver
     @driver.get 'http://www.sport195.com/#'
     @hash_elements = {
         sign_up: {xpath: ".//*[@class='splash']//a[@class='btn btn-danger btn-sign-up']"},
@@ -11,20 +14,46 @@ class HomePageUI < BasePage
     }
   end
 
+  def init_sign_up
+    if @sign_up_button.nil?
+      begin
+        @sign_up_button = AbstractElement.new(@driver.find_element(@hash_elements[:sign_up]))
+      rescue
+        return nil
+      end
+    else
+      return @sign_up_button
+    end
+  end
+
+  def init_log_in
+    if @log_in_button.nil?
+      begin
+        @log_in_button = AbstractElement.new(@driver.find_element(@hash_elements[:log_in]))
+      rescue
+        return nil
+      end
+    else
+      return @log_in_button
+    end
+  end
+
   def click_sign_up
-    element = @driver.find_element(@hash_elements[:sign_up])
-    element.click
+    elem = init_sign_up
+    elem.click
   end
 
   def click_log_in
-    element = @driver.find_element(@hash_elements[:log_in])
+    element = init_log_in
     element.click
   end
 
   def exist
-   element1 = @driver.find_element(@hash_elements[:sign_up])
-   element2 = @driver.find_element(@hash_elements[:log_in])
-   return exist_base(element1) && exist_base(element2)
+    if init_sign_up.nil? || init_log_in.nil?
+      return false
+    else
+      return init_sign_up.present? && init_log_in.present?
+    end
   end
 
 end

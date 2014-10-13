ceb460ded25cc6d049af449055a74e7c46ebf8e6
Yaroslav <slav4yk@gmail.com>
2014-10-13 15:43:50 +0300

    delete method :
      def exist_base(element)
        @value = true
        begin
          @identifier_of_page = element.displayed?
          @value = false
        rescue
          if @value
            puts "Element does not exist"
            @value = true
          end
        end
    
        status = @identifier_of_page
        print "element is displayed? :  "
        puts(status ? 'Yes' : 'No')
    
        return status
      end

--- lib/uimap/base_page.rb
+++ lib/uimap/base_page.rb
@@ -8,23 +8,4 @@ class BasePage
     @driver = driver_instance
   end
 
-  def exist_base(element)
-    @value = true
-    begin
-      @identifier_of_page = element.displayed?
-      @value = false
-    rescue
-      if @value
-        puts "Element does not exist"
-        @value = true
-      end
-    end
-
-    status = @identifier_of_page
-    print "element is displayed? :  "
-    puts(status ? 'Yes' : 'No')
-
-    return status
-  end
-
 end

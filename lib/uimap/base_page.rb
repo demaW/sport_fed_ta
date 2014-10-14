471cffba015604736a8b6919b170ef0f5639af27
Yaroslav <slav4yk@gmail.com>
2014-10-14 13:18:32 +0300

    edit path to driver

--- lib/uimap/base_page.rb
+++ lib/uimap/base_page.rb
@@ -1,10 +1,10 @@
-require_relative('../../lib/driver/driverbla')
+require_relative('../../lib/driver/driver_type')
 require 'selenium-webdriver'
 
 class BasePage
   attr_accessor :driver
   def initialize()
-    driver_instance = Driver.new()
+    driver_instance = DriverType.new()
     @driver = driver_instance
   end
 

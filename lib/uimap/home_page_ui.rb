5dbc6bf5340fcd87abd247357e0c08e85533a75f
Yaroslav <slav4yk@gmail.com>
2014-10-14 13:20:46 +0300

    edit exist method and init methods

--- lib/uimap/home_page_ui.rb
+++ lib/uimap/home_page_ui.rb
@@ -6,40 +6,27 @@ class HomePageUI < BasePage
   attr_accessor :hash_elements
 
   def initialize
-    @driver = super.driver
+    @driver = super.init
+#    @driver = Selenium::WebDriver.for :firefox
     @driver.get 'http://www.sport195.com/#'
     @hash_elements = {
-        sign_up: {xpath: ".//*[@class='splash']//a[@class='btn btn-danger btn-sign-up1']"},
+        sign_up: {xpath: ".//*[@class='splash']//a[@class='btn btn-danger btn-sign-up']"},
         log_in: {xpath: ".//*[@class='splash']//a[@class='btn btn-primary btn-login']"}
     }
   end
 
   def init_sign_up
     if @sign_up_button.nil?
-      begin
-        @sign_up_button = AbstractElement.new(@driver.find_element(@hash_elements[:sign_up]))
-      rescue Exception => e
-        $logger.error "#{__method__} not found element"
-        $logger.error "#{e.message}"
-        return nil
-      end
-    else
-      return @sign_up_button
+      @sign_up_button = AbstractElement.new(@driver, @hash_elements[:sign_up])
     end
+    return @sign_up_button
   end
 
   def init_log_in
     if @log_in_button.nil?
-      begin
-        @log_in_button = AbstractElement.new(@driver.find_element(@hash_elements[:log_in]))
-      rescue
-        $logger.error "#{__method__} not found element"
-        $logger.error "#{e.message}"
-        return nil
-      end
-    else
-      return @log_in_button
+      @log_in_button = AbstractElement.new(@driver, @hash_elements[:log_in])
     end
+    return @log_in_button
   end
 
   def click_sign_up
@@ -53,10 +40,10 @@ class HomePageUI < BasePage
   end
 
   def exist
-    if init_sign_up.nil? || init_log_in.nil?
-      return false
+    if (init_sign_up.is_element_found && init_log_in.is_element_found)
+      return @log_in_button.displayed? && @sign_up_button.displayed?
     else
-      return init_sign_up.displayed? && init_log_in.displayed?
+      return false
     end
   end
 

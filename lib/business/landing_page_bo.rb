e1e60b0acd031dfcab77b143bd9adecd7d9728f0
demaW <demavov@gmail.com>
2014-10-14 18:16:51 +0300

    refactor driver, landing_page_ui renamed

--- lib/business/landing_page_bo.rb
+++ lib/business/landing_page_bo.rb
@@ -1,22 +1,20 @@
 require "selenium-webdriver"
-
-require_relative '../uimap/landinng_page_ui'
+require_relative '../../lib/test/base_test'
 class LandingPageBO
-  def initialize(driver)
-    @driver = driver
-    @landing_page_elements = LandinngPageUI.new
+  def initialize
+    @landing_page_elements = LandingPageUI.new
   end
 
   def login
-    singn_in = @driver.find_element(@landing_page_elements.hashElement[:login_button])
-    singn_in.click()
-    login_field = @driver.find_element(@landing_page_elements.hashElement[:input_login])
-    login_field.send_keys("volodymyrdemchyk@gmail.com")
-    pass_field = @driver.find_element(@landing_page_elements.hashElement[:input_password])
-    pass_field.send_keys("181992")
+    sign_in = $driver.find_element(@landing_page_elements.hashElement[:login_button])
+    sign_in.click
+    login_field = $driver.find_element(@landing_page_elements.hashElement[:input_login])
+    login_field.send_keys 'volodymyrdemchyk@gmail.com'
+    pass_field = $driver.find_element(@landing_page_elements.hashElement[:input_password])
+    pass_field.send_keys '181992'
     wait = Selenium::WebDriver::Wait.new(:timeout => 10)
-    wait.until{@driver.find_element(@landing_page_elements.hashElement[:login_submit])}
-    login_submit = @driver.find_element(@landing_page_elements.hashElement[:login_submit])
+    wait.until{ $driver.find_element(@landing_page_elements.hashElement[:login_submit])}
+    login_submit = $driver.find_element(@landing_page_elements.hashElement[:login_submit])
     login_submit.click
   end
 end

8c4930b9f31773cd457934220cded621edc7e665
Yaroslav <slav4yk@gmail.com>
2014-10-14 13:22:21 +0300

    add find_element method

--- lib/elements/abstract_element.rb
+++ lib/elements/abstract_element.rb
@@ -1,21 +1,34 @@
 require 'selenium-webdriver'
 require_relative '../utils/log'
 class AbstractElement
-
-  def initialize(element)
-   @element = element
-
+  attr_accessor :is_element_found
+  def initialize(driver, element_locator)
+    @is_element_found = false
+    @driver = driver
+    @element = find_element(element_locator)
   end
 
   def click
-  @element.click
-  $logger.info "#{__method__} on element"
+    @element.click
+    $logger.info "#{__method__} on element"
   end
 
   def displayed?
     status = @element.displayed?
     $logger.info "element is displayed? :  #{status}"
     return status
+  end
+
+  def find_element(element_locator)
+    begin
+      @is_element_found = true
+      return @driver.find_element(element_locator)
+    rescue Exception => e
+      @is_element_found = false
+      $logger.error "#{__method__} method in abstract_element class: Element not found !"
+      $logger.error "#{e.message}"
+      return nil
+    end
   end
 
 end
